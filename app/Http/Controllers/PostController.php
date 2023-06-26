3ed59b23cadc9735e8bc7df6aecbfce32e62420e
EC2 Default User <masa.110804@gmail.com>
2023-06-27 02:41:22 +0900

    8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
+   public function index(Post $post)
     {
-         return $post->get();
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        // getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
-｝
+    public function show(Post $post)
+{
+    return view('posts.show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
+}
+?>
